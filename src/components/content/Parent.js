ac6737b08583ba5191c63524a6c6e40f38e3a722
Kenne Serge <kenne_serge@mail.ru>
2020-09-07 04:32:04 +0300

    add serve

--- src/components/content/Parent.js
+++ src/components/content/Parent.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import{locationContext} from '../../context';
 import Cart from './Cart'
 import ParentChildren from './ParentChildren';
